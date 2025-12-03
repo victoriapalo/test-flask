e8ce5fb237da059a86bf9820794bcfd93b9fc229
Victoria Palozzi <vp238423@ohio.edu>
2025-12-02 22:39:24 -0500

    Add uvicorn.run() for running FastAPI via python main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional, List 
+import uvicorn 
 
 app = FastAPI()
 
@@ -81,3 +82,6 @@ def delete_item(item_id: int):
 def delete_all_items():
     items.clear()
     return
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

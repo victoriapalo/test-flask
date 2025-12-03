cbe0f6161d7fd7c15b34e32307fe539fb1725ca3
Ubuntu <ubuntu@ip-172-31-44-154.us-east-2.compute.internal>
2025-12-03 15:51:23 +0000

    Add FastAPI app with Nginx and systemd configs

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/victoria/CS3610/apiassignment/venv
+set -gx VIRTUAL_ENV /home/ubuntu/test-flask/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
